f14095dcd69e9fa2d27f5f8c8451887da4f4b8e4
JoaHoe/buildenh_jh <joachim.hoehle@outlook.dk>
2025-06-27 11:58:56 +0200

    Create func_loadLib_jh.R

---
+++ R/func/func_loadLib_jh.R
@@ -0,0 +1,42 @@
+##name of script: func_loadLib_jh.R
+#purpose: collection of all functions for package 'buildenh'
+#author: Joachim Höhle
+#GNU General Public License (GPL)
+
+loadLib_jh <- function() { 
+  source("./func/func_adjust_coord.R")
+  source("./func/func_adj_orthog_corcoo.R")
+  source("./func/func_changeDir.R")
+  source("./func/func_corner_name.R")
+  source("./func/func_design_mat.R")
+  source("./func/func_det_of_angle.R")
+  source("./func/func_det_scale.R")
+  source("./func/func_detect_meas1.R")
+  source("./func/func_dist_PC.R")
+  source("./func/func_dist_v.R")
+  source("./func/func_dist_v2.R")
+  source("./func/func_fe.R")
+  source("./func/func_hist_lin_len.R")
+  source("./func/func_line_reduce.R")
+  source("./func/func_locator2.R")
+  source("./func/func_mean_line.R")
+  source("./func/func_new_centre.R")
+  source("./func/func_new_centre_auto.R")
+  source("./func/func_PC_segment_4.R")
+  source("./func/func_plot_line_segment.R")
+  source("./func/func_plot_PC.R")
+  source("./func/func_plot_PC_all.R")
+  source("./func/func_plot_PC_2scales.R")
+  source("./func/func_rectang_lines.R")
+  source("./func/func_reduce_pointset.R")
+  source("./func/func_ro3_from_xy.R")
+  source("./func/func_test_gaps_onedirection.R")
+  source("./func/func_detect_thet_len.R")
+  source("./func/func_trans_ortho.R")
+  source("./func/func_trans_H_res.R")
+  source("./func/func_w_av.R")
+} #end of function 'loadLib_jh()'
+
+#end of script 'func_loadLib_jh.R'
+
+################################################################################
