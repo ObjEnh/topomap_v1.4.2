ad246ad5f3b48034e0bc3a8d5969dff04069b642
JoaHoe/buildenh_jh <joachim.hoehle@outlook.dk>
2025-09-29 09:25:05 +0200

    Update func_fe.R

--- R/func/func_fe.R
+++ R/func/func_fe.R
@@ -4,17 +4,19 @@
 ##arguments:
 #wd2: length of line 
 #ces2: number of ortholines at the 8 longest lines
-#nonortho: number of nonortholines at the 8 longest lines 
+#nonortho: number of nonortholines at the 8 longest lines
+#n_pix8: number of pixels at 8th longest line segment (threshold)
 ##author: Joachim Höhle
 ##GNU General Public License (GPL)
 
 fe <- function(wd2,ces2,nonortho) { 
-  q <- 0
+  q <- 5 #nonortho_only_RDP
   ##parameters for selection of object-type
-  if (wd2 <= 80 && ces2 >= 1 && nonortho >= 8) {q <- 1} # "extr_wd"
-  if (wd2 > 80 && ces2 >= 3 && nonortho >= 6) {q <- 2} # "4_long"
-  if (wd2 > 80 && ces2 >= 2 && nonortho >= 7) {q <- 3} # "100_all"
-  if (wd2 > 80 && ces2 >= 1 && nonortho >= 8) {q <- 4} # "100_all+nonortho"
+  if (wd2 <= n_pix8 && ces2 >= 4 && nonortho >= 4) {q <- 1} # "extr_wd"
+  if (wd2 >  n_pix8 && ces2 >= 3 && nonortho >= 5) {q <- 2} # "4_long"
+  if (wd2 >  n_pix8 && ces2 >= 2 && nonortho >= 6) {q <- 3} # "100_all"
+  if (wd2 >  n_pix8 && ces2 >= 1 && nonortho >= 7) {q <- 4} # "100_all+nonortho"
+  if (wd2 >  n_pix8 && ces2 == 1 && nonortho == 7) {q <- 5} # "nonortho_only"
   return(q) 
 } #end of function 'fe'
 
