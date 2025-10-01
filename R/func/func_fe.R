fa9ddf32518579051be6df388e11ed9966dab63a
JoaHoe/buildenh_jh <joachim.hoehle@outlook.dk>
2025-10-01 20:32:04 +0200

    Update func_fe.R

--- R/func/func_fe.R
+++ R/func/func_fe.R
@@ -12,11 +12,11 @@
 fe <- function(wd2,ces2,nonortho) { 
   q <- 5 #"nonortho_only"
   ##parameters for selection of object-type
-  if (wd2 <= n_pix8 && ces2 >= 4 && nonortho >= 4) {q <- 1} # "extr_wd"
-  if (wd2 >  n_pix8 && ces2 >= 3 && nonortho >= 5) {q <- 2} # "4_long"
-  if (wd2 >  n_pix8 && ces2 >= 2 && nonortho >= 6) {q <- 3} # "100_all"
-  if (wd2 >  n_pix8 && ces2 >= 1 && nonortho >= 7) {q <- 4} # "100_all+nonortho"
-  if (wd2 >  n_pix8 && ces2 == 1 && nonortho == 7) {q <- 6} # "nonortho_only_RDP"
+  if (wd2 < n_pix8 && ces2 >= 4 && nonortho >= 4) {q <- 1} # "extr_wd"
+  if (wd2 >=  n_pix8 && ces2 >= 3 && nonortho >= 5) {q <- 2} # "4_long"
+  if (wd2 >=  n_pix8 && ces2 >= 2 && nonortho >= 6) {q <- 3} # "100_all"
+  if (wd2 >=  n_pix8 && ces2 >= 1 && nonortho >= 7) {q <- 4} # "100_all+nonortho"
+  if (wd2 >=  n_pix8 && ces2 == 1 && nonortho == 7) {q <- 6} # "nonortho_only_RDP"
   return(q) 
 } #end of function 'fe'
 
